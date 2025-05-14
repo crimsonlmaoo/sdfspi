31989c84c91fdacff475b4f9ede624d1f845bbdf
crimson <151975517+crimsonlmaoo@users.noreply.github.com>
2025-05-14 18:37:07 -0500

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', marginTop: '100px' }}>
+      <h1>running</h1>
+      <p>api gotta work :shrug:</p>
+      <code>/api/users</code>
+    </div>
+  );
+}
